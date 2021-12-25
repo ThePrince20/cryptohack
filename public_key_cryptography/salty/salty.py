fb2cd0c8d8df387590c692c20706f7336ebdddf9
ThePrince20 <b00129500@mytudublin.ie>
2021-12-24 22:38:04 -0500

    25/12/21

---
+++ public_key_cryptography/salty/salty.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from Crypto.Util.number import getPrime, inverse, bytes_to_long, long_to_bytes
+
+e = 1
+d = -1
+
+while d == -1:
+    p = getPrime(512)
+    q = getPrime(512)
+    phi = (p - 1) * (q - 1)
+    d = inverse(e, phi)
+
+n = p * q
+
+flag = b"XXXXXXXXXXXXXXXXXXXXXXX"
+pt = bytes_to_long(flag)
+ct = pow(pt, e, n)
+
+print(f"n = {n}")
+print(f"e = {e}")
+print(f"ct = {ct}")
+
+pt = pow(ct, d, n)
+decrypted = long_to_bytes(pt)
+assert decrypted == flag
+
+
+ct = 44981230718212183604274785925793145442655465025264554046028251311164494127485
+hexed_ct = hex(ct)
+
+flag = bytearray.fromhex(hexed_ct[2:]).decode() # hexed_ct[2:] remove '0x' from the beginning
+
+print(flag)
